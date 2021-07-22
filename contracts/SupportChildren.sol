058e9cdd74edc6dd459f2b9ed4e99b5fb1bdb7ab
Zarko Jevtic <zarko.jevtic@appmodule.net>
2021-07-22 21:10:02 +0200

    Added events to contract and send collected money to beneficiarry account

--- contracts/SupportChildren.sol
+++ contracts/SupportChildren.sol
@@ -2,6 +2,18 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract SupportChildren {
+
+    event DonationMade (
+        uint camppaignId,
+        uint donationAmount,
+        string notifyEmail
+    );
+
+    event CampaignFinished (
+        uint camppaignId,
+        uint currentAmount,
+        string[] notifyEmails
+    );
     
     uint campaignIndex;
     uint count;
@@ -13,14 +25,14 @@ contract SupportChildren {
         string creatorEmail; 
         uint targetAmount;
         uint currentAmount;
-        address targetAccount;
+        address payable targetAccount;
     }
     
     Campaign[] campaigns;
     
     mapping(uint => string[]) campaignDonationsEmails;
     
-    function createCampaign(string memory _name, string memory _description, string memory _creatorEmail, uint32 _targetAmount, address _targetAccount) public {
+    function createCampaign(string memory _name, string memory _description, string memory _creatorEmail, uint32 _targetAmount, address payable _targetAccount) public {
          campaigns.push(Campaign({
             id: count,
             name: _name,
@@ -34,17 +46,25 @@ contract SupportChildren {
         count++;
     }
     
-    // Fiktivno slanje para za test samo
+    // Only for test
     function donateTest(uint _campaignId, string memory _donorEmail, uint _amount) public {
         campaignDonationsEmails[_campaignId].push(_donorEmail);
         
         campaigns[_campaignId].targetAmount = campaigns[_campaignId].currentAmount + _amount;
     }
 
-    // Stvarno slanje ETH
+    // Real ETH donation
     function donate(uint _campaignId, string memory _donorEmail) payable public {
         campaignDonationsEmails[_campaignId].push(_donorEmail);
         campaigns[_campaignId].currentAmount = campaigns[_campaignId].currentAmount  + msg.value;
+
+        emit DonationMade(_campaignId, msg.value, campaigns[_campaignId].creatorEmail);
+
+        if (campaigns[_campaignId].currentAmount >= campaigns[_campaignId].targetAmount) {
+            emit CampaignFinished(_campaignId, campaigns[_campaignId].currentAmount, campaignDonationsEmails[_campaignId]);
+            // Send ETH to the address of the child
+            campaigns[_campaignId].targetAccount.transfer(campaigns[_campaignId].currentAmount);
+        }
     }
     
     function getDonatorEmails(uint _campaignId) view public returns (string[] memory) {
